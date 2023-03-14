a5f384863d7a83b641a24bf0ba4d543a355a1221
notsatvrn <pastawho@gmail.com>
2023-03-14 01:12:17 +0000

    feat: utils.respring(), more IOSurface definitions, cleaner APIs, etc

--- src/utils.zig
+++ src/utils.zig
@@ -1,20 +1,37 @@
 const versioning = @import("versioning.zig");
-//const objc = @import("objc");
+const objc = @import("objc");
 const std = @import("std");
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-pub var alloc = gpa.allocator();
+//var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+//pub var alloc = gpa.allocator();
+pub var alloc = std.heap.c_allocator;
 
-// WIP
 // https://adamdemasi.com/2019/08/25/respring-the-right-way-followup.html
 pub fn respring() void {
-    //const SBSRelaunchAction = objc.Class.GetClass("SBSRelaunchAction").?;
+    var relaunch_action = objc.Object{};
 
     if (versioning.versionAtLeast(versioning.getVersion(), .{9,3,0})) {
-        // 9.3+
+        var SBSRelaunchAction = objc.Class.GetClass("SBSRelaunchAction").?;
+        var SBSRelaunchActionOptionsFadeToBlackTransition = objc.Class.GetClass("SBSRelaunchActionOptionsFadeToBlackTransition").?;
+        relaunch_action = SBSRelaunchAction.msgSend(
+            objc.Object,
+            objc.Sel("actionWithReason:options:targetURL:"),
+            .{"RelaunchServerAction", SBSRelaunchActionOptionsFadeToBlackTransition, null},
+        );
     } else {
-        // 8.0 - 9.2
+        var SBSRestartRenderServerAction = objc.Class.GetClass("SBSRestartRenderServerAction").?;
+        relaunch_action = SBSRestartRenderServerAction.msgSend(
+            objc.Object,
+            objc.Sel("restartActionWithTargetRelaunchURL:"),
+            .{null},
+        );
     }
+
+    var FBSSystemService = objc.Class.getClass("FBSSystemService").?;
+    var shared_service = FBSSystemService.msgSend(objc.Object, objc.sel("sharedService"), .{});
+    var NSSet = objc.Class.getClass("NSSet").?;
+    var action_set = NSSet.msgSend(objc.Object, objc.Sel("setWithObject:"), .{relaunch_action});
+    shared_service.msgSend(void, objc.Sel("sendActions:withResult:"), .{action_set, null});
 }
 
 pub fn errorf(comptime fmt: []const u8, args: anytype) void {
